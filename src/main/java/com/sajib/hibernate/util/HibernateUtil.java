7b153c0b457b4f7d6dd358b322ec23b8671c7ffd
sajibDev <mdsajibcse14@gmail.com>
2024-02-14 23:07:25 +0600

    session factory manual scan added

--- src/main/java/com/sajib/hibernate/util/HibernateUtil.java
+++ src/main/java/com/sajib/hibernate/util/HibernateUtil.java
@@ -1,19 +1,23 @@
 package com.sajib.hibernate.util;
 
 
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
-
     public static SessionFactory getSessionFactory(String configFile) {
-
-
-
 
         if (sessionFactory == null) {
             configFile += ".cfg.xml";
@@ -22,12 +26,34 @@ public class HibernateUtil {
                     .configure(configFile)
                     .build();
             MetadataSources metadataSources = new MetadataSources(standardRegistry);
-            metadataSources.addPackage("com.sajib.hibernate.model");
+
+            Set<Class> classes = findAllClassesUsingClassLoader("com.sajib.hibernate.model");
+            for(Class item:classes) metadataSources.addAnnotatedClass(item);
             sessionFactory = metadataSources.buildMetadata().buildSessionFactory();
         }
 
         return sessionFactory;
 
+    }
+
+    public static Set<Class> findAllClassesUsingClassLoader(String packageName) {
+        InputStream stream = ClassLoader.getSystemClassLoader()
+                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        return reader.lines()
+                .filter(line -> line.endsWith(".class"))
+                .map(line -> getClass(line, packageName))
+                .collect(Collectors.toSet());
+    }
+
+    private static Class getClass(String className, String packageName) {
+        try {
+            return Class.forName(packageName + "."
+                    + className.substring(0, className.lastIndexOf('.')));
+        } catch (ClassNotFoundException e) {
+            // handle the exception
+        }
+        return null;
     }
 
 }
